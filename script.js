ad7884ab15c0412bfd64c1fbb66eb82f60d71bfe
toSteven <tostevenhoward@gmail.com>
2023-10-14 00:59:31 +0800

    add ui & catch

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
-const API_KEY = "53b63c2e08638236568f32b10f2aa5f3";
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const API_KEY = "53b63c2e08638236568f32b10f2aa5f3"; // api key
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q="; // api url
+let targetCity = (document.querySelector('#search_input')); // search input
 
+// target is parameter
 function OpenWeather(target) {
-    fetch(`${API_URL}${target}&appid=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
+    fetch(`${API_URL}${target}&appid=${API_KEY}`) // fetch api url + target paramete + api key
+        .then(response => response.json()) // set as json
+        .then(data => { // set data as temp variables
+            //console.log(data)
 
+            // get elements and insert data
             document.querySelector('#weatherStatus').innerHTML = data.weather[0].main;
             document.querySelector('#weatherDesc').innerHTML = data.weather[0].description;
             document.querySelector('#city').innerHTML = data.name;
             document.querySelector('#country').innerHTML = data.sys.country;
-            document.querySelector('#temp').innerHTML = Math.round(data.main.temp);
-            document.querySelector('#humid').innerHTML = data.main.humidity;
-            document.querySelector('#windSpd').innerHTML = data.wind.speed;
+            document.querySelector('#temp').innerHTML = Math.round(data.main.temp) + "°C";
+            document.querySelector('#humid').innerHTML = data.main.humidity + " %";
+            document.querySelector('#windSpd').innerHTML = data.wind.speed + " kp/h";
+
+            // get element and set to temp variable
             let Weather_Img = document.querySelector('#weatherImg');
 
+            // weather imd conditions
             if (data.weather[0].main == "Clouds") {
-                Weather_Img.src = 'img/clouds.png';
+                Weather_Img.src = "imgs/clouds.png";
             } else if (data.weather[0].main == "Rain") {
-                Weather_Img.src = 'img/rain.png';
+                Weather_Img.src = "imgs/rain.png";
             } else if (data.weather[0].main == "Clear") {
-                Weather_Img.src = 'img/clear.png';
+                Weather_Img.src = "imgs/clear.png";
             } else if (data.weather[0].main == "Snow") {
-                Weather_Img.src = 'img/snow.png';
+                Weather_Img.src = "imgs/snow.png";
             } else if (data.weather[0].main == "Mist") {
-                Weather_Img.src = 'img/mist.png';
+                Weather_Img.src = "imgs/mist.png";
             } else if (data.weather[0].main == "Drizzle") {
-                Weather_Img.src = 'img/drizzle.png';
+                Weather_Img.src = "imgs/drizzle.png";
             }
 
-        })
-}
+            // shot data display
+            document.querySelector('#data').style.display = "block";
 
-let targetCity = (document.querySelector('#search_input'));
+            // clear input
+            targetCity.value = '';
+        })
+        .catch(error => {
+            // error
+            alert('City not found');
+            location.reload();
+        });
+}
+// button click event
 document.querySelector('#search_button').addEventListener('click', () => {
-    
-    if (targetCity.value){
-     OpenWeather(targetCity.value);
-    }else{
+    // input condition
+    if (targetCity.value) {
+        OpenWeather(targetCity.value);
+    } else {
         alert("Enter City");
     }
-
 
 });

738aad32ed8f711799354035528cafc0567c47fd
CyEvan <isayascyrilleevan@gmail.com>
2023-10-13 23:34:53 +0800

    Added logic

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const API_KEY = "53b63c2e08638236568f32b10f2aa5f3";
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=manila";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-function OpenWeather() {
-    fetch(`${API_URL}&appid=${API_KEY}`)
+function OpenWeather(target) {
+    fetch(`${API_URL}${target}&appid=${API_KEY}`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -14,7 +14,33 @@ function OpenWeather() {
             document.querySelector('#temp').innerHTML = Math.round(data.main.temp);
             document.querySelector('#humid').innerHTML = data.main.humidity;
             document.querySelector('#windSpd').innerHTML = data.wind.speed;
+            let Weather_Img = document.querySelector('#weatherImg');
+
+            if (data.weather[0].main == "Clouds") {
+                Weather_Img.src = 'img/clouds.png';
+            } else if (data.weather[0].main == "Rain") {
+                Weather_Img.src = 'img/rain.png';
+            } else if (data.weather[0].main == "Clear") {
+                Weather_Img.src = 'img/clear.png';
+            } else if (data.weather[0].main == "Snow") {
+                Weather_Img.src = 'img/snow.png';
+            } else if (data.weather[0].main == "Mist") {
+                Weather_Img.src = 'img/mist.png';
+            } else if (data.weather[0].main == "Drizzle") {
+                Weather_Img.src = 'img/drizzle.png';
+            }
+
         })
 }
 
-OpenWeather();
+let targetCity = (document.querySelector('#search_input'));
+document.querySelector('#search_button').addEventListener('click', () => {
+    
+    if (targetCity.value){
+     OpenWeather(targetCity.value);
+    }else{
+        alert("Enter City");
+    }
+
+
+});
